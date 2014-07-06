d0f4763512ca7dfb3fe114292a2992ab8f78cfc4
Thomas55170 <tberthemin@yahoo.com>
2014-07-06 22:24:46 +0200

    test gitignore

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,12 +9,6 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("connexion.fxml"));
-        primaryStage.setTitle("SQLTool - Connexion");
-        primaryStage.setScene(new Scene(root, 290, 349));
-        primaryStage.show();
-    }
 
     public static void main(String[] args) {
         launch(args);
